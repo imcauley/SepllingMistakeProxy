aef5b14eafe21e81c2b4d979254b3c57ea137b35
Isaac McAuley <mcauleyisaac@gmail.com>
2019-05-10 19:24:04 -0600

    fixed server loop

--- temp_server.cpp
+++ temp_server.cpp
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char *hello = "HTTP/1.1 200 OK\n\nHello from server";
+    char hello[] = "HTTP/1.1 200 OK\n\nHello from server";
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -34,25 +34,34 @@ int main(int argc, char const *argv[])
     address.sin_port = htons( PORT );
 
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
+
     if (listen(server_fd, 3) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+
+    while(1)
     {
-        perror("accept");
-        exit(EXIT_FAILURE);
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read(new_socket , buffer, 1024);
+
+        //do shit
+        printf("%s\n",buffer );
+
+        send(new_socket , hello , strlen(hello) , 0);
+        printf("Hello message sent\n");
+        close(new_socket);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
     return 0;
 }
